69aff8476d4d4ce72b3cfff54f7a38581496bdb1
nassiralawar <nassir.alawar@gmail.com>
2020-06-05 09:42:37 -0700

    Updated versions and androidx support

--- app/src/main/java/com/nalawar/genform/BaseGenFormAdapter.kt
+++ app/src/main/java/com/nalawar/genform/BaseGenFormAdapter.kt
@@ -1,8 +1,8 @@
 package com.nalawar.genform
 
 import android.content.Context
-import android.databinding.ViewDataBinding
-import android.support.v7.widget.RecyclerView
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 
 /**
